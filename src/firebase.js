a391fe15345d78928b57d634778b5d60451331cc
Harsh Shah <harsh.r.shah2102@gmail.com>
2021-06-22 12:06:09 +0530

    Deploying

--- src/firebase.js
+++ src/firebase.js
@@ -4,24 +4,24 @@ import "firebase/firestore";
 import "firebase/storage";
 
 // For production
-// const app = firebase.initializeApp({
-// apiKey: "AIzaSyDRThStYSBq5P2DWJExJJmZ4j2TgmrYtP8",
-// authDomain: "knox-drive-production.firebaseapp.com",
-// projectId: "knox-drive-production",
-// storageBucket: "knox-drive-production.appspot.com",
-// messagingSenderId: "89891201731",
-// appId: "1:89891201731:web:054e2651f8c6ba62567876"
-// })
+const app = firebase.initializeApp({
+  apiKey: "AIzaSyDRThStYSBq5P2DWJExJJmZ4j2TgmrYtP8",
+  authDomain: "knox-drive-production.firebaseapp.com",
+  projectId: "knox-drive-production",
+  storageBucket: "knox-drive-production.appspot.com",
+  messagingSenderId: "89891201731",
+  appId: "1:89891201731:web:054e2651f8c6ba62567876",
+});
 
 // For developement
-const app = firebase.initializeApp({
-  apiKey: "AIzaSyBjsDMKT4OqxrrpbBQyHb5fQHv2hj2PPh4",
-  authDomain: "knox-drive-developement.firebaseapp.com",
-  projectId: "knox-drive-developement",
-  storageBucket: "knox-drive-developement.appspot.com",
-  messagingSenderId: "485993244362",
-  appId: "1:485993244362:web:59007e59746719e38ff633",
-});
+// const app = firebase.initializeApp({
+//   apiKey: "AIzaSyBjsDMKT4OqxrrpbBQyHb5fQHv2hj2PPh4",
+//   authDomain: "knox-drive-developement.firebaseapp.com",
+//   projectId: "knox-drive-developement",
+//   storageBucket: "knox-drive-developement.appspot.com",
+//   messagingSenderId: "485993244362",
+//   appId: "1:485993244362:web:59007e59746719e38ff633",
+// });
 
 const firestore = app.firestore();
 export const database = {
