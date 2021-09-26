8cff5ef3c8e70913d1bc746164663ed34adb45ba
Harsh-0986 <harsh.r.shah2102@gmail.com>
2021-09-26 15:39:35 +0530

    Added dark mode to signup screen

--- src/components/authentication/Signup.js
+++ src/components/authentication/Signup.js
@@ -13,6 +13,7 @@ export default function Signup() {
   const [message, setMessage] = useState("");
   const [loading, setLoading] = useState(false);
   const history = useHistory();
+  const darkMode = JSON.parse(localStorage.getItem("darkMode"));
 
   async function handleSubmit(e) {
     e.preventDefault();
@@ -34,34 +35,40 @@ export default function Signup() {
   }
 
   return (
-    <CenteredContainer>
-      <Card>
-        <Card.Body>
-          <h2 className="text-center mb-4">Sign Up</h2>
-          {error && <Alert variant="danger">{error}</Alert>}
-          {message && <Alert variant="success">{message}</Alert>}
-          <Form onSubmit={handleSubmit}>
-            <Form.Group id="email">
-              <Form.Label>Email</Form.Label>
-              <Form.Control type="email" ref={emailRef} required />
-            </Form.Group>
-            <Form.Group id="password">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" ref={passwordRef} required />
-            </Form.Group>
-            <Form.Group id="password-confirm">
-              <Form.Label>Confirm password</Form.Label>
-              <Form.Control type="password" ref={passwordConfirmRef} required />
-            </Form.Group>
-            <Button disabled={loading} className="w-100 mt-3" type="submit">
-              Sign Up
-            </Button>
-          </Form>
-        </Card.Body>
-      </Card>
-      <div className="w-100 text-center m-2">
-        Already have an account? <Link to="/login">Log In</Link>
-      </div>
-    </CenteredContainer>
+    <div className={darkMode ? "bg-dark text-white" : "bg-white"}>
+      <CenteredContainer>
+        <Card className={darkMode ? "bg-dark" : "bg-light"}>
+          <Card.Body>
+            <h2 className="text-center mb-4">Sign Up</h2>
+            {error && <Alert variant="danger">{error}</Alert>}
+            {message && <Alert variant="success">{message}</Alert>}
+            <Form onSubmit={handleSubmit}>
+              <Form.Group id="email">
+                <Form.Label>Email</Form.Label>
+                <Form.Control type="email" ref={emailRef} required />
+              </Form.Group>
+              <Form.Group id="password">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" ref={passwordRef} required />
+              </Form.Group>
+              <Form.Group id="password-confirm">
+                <Form.Label>Confirm password</Form.Label>
+                <Form.Control
+                  type="password"
+                  ref={passwordConfirmRef}
+                  required
+                />
+              </Form.Group>
+              <Button disabled={loading} className="w-100 mt-3" type="submit">
+                Sign Up
+              </Button>
+            </Form>
+          </Card.Body>
+        </Card>
+        <div className="w-100 text-center m-2">
+          Already have an account? <Link to="/login">Log In</Link>
+        </div>
+      </CenteredContainer>
+    </div>
   );
 }
