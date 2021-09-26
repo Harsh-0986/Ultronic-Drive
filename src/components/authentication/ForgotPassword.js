5177b590837820e70ac0364932a5c223e0d0cff0
Harsh-0986 <harsh.r.shah2102@gmail.com>
2021-09-26 15:37:56 +0530

    Added dark mode to forgot password screen

--- src/components/authentication/ForgotPassword.js
+++ src/components/authentication/ForgotPassword.js
@@ -10,6 +10,7 @@ export default function ForgotPassword() {
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
   const [message, setMessage] = useState("");
+  const darkMode = JSON.parse(localStorage.getItem("darkMode"));
 
   async function handleSubmit(e) {
     e.preventDefault();
@@ -27,30 +28,32 @@ export default function ForgotPassword() {
   }
 
   return (
-    <CenteredContainer>
-      <Card>
-        <Card.Body>
-          <h2 className="text-center mb-4">Password Reset</h2>
-          {error && <Alert variant="danger">{error}</Alert>}
-          {message && <Alert variant="success">{message}</Alert>}
-          <Form onSubmit={handleSubmit}>
-            <Form.Group id="email">
-              <Form.Label>Email</Form.Label>
-              <Form.Control type="email" ref={emailRef} required />
-            </Form.Group>
+    <div className={darkMode ? "text-white bg-dark" : "bg-white"}>
+      <CenteredContainer>
+        <Card className={darkMode ? "bg-dark" : "bg-light"}>
+          <Card.Body>
+            <h2 className="text-center mb-4">Password Reset</h2>
+            {error && <Alert variant="danger">{error}</Alert>}
+            {message && <Alert variant="success">{message}</Alert>}
+            <Form onSubmit={handleSubmit}>
+              <Form.Group id="email">
+                <Form.Label>Email</Form.Label>
+                <Form.Control type="email" ref={emailRef} required />
+              </Form.Group>
 
-            <Button disabled={loading} className="w-100 mt-3" type="submit">
-              Reset Password
-            </Button>
-          </Form>
-          <div className="w-100 text-center mt-3">
-            <Link to="/login">Login</Link>
-          </div>
-        </Card.Body>
-      </Card>
-      <div className="w-100 text-center m-2">
-        Need an account? <Link to="/signup">Sign Up</Link>
-      </div>
-    </CenteredContainer>
+              <Button disabled={loading} className="w-100 mt-3" type="submit">
+                Reset Password
+              </Button>
+            </Form>
+            <div className="w-100 text-center mt-3">
+              <Link to="/login">Login</Link>
+            </div>
+          </Card.Body>
+        </Card>
+        <div className="w-100 text-center m-2">
+          Need an account? <Link to="/signup">Sign Up</Link>
+        </div>
+      </CenteredContainer>
+    </div>
   );
 }
