81b5b47c2e6a56e00590ae7b9aaa7f92c62fa68f
Harsh-0986 <harsh.r.shah2102@gmail.com>
2021-09-26 15:36:35 +0530

    Added dark mode to login screen

--- src/components/authentication/CenteredContainer.js
+++ src/components/authentication/CenteredContainer.js
@@ -2,9 +2,12 @@ import React from "react";
 import { Container } from "react-bootstrap";
 
 export default function CenteredContainer({ children }) {
+  const darkMode = JSON.parse(localStorage.getItem("darkMode"));
   return (
     <Container
-      className="d-flex align-items-center justify-content-center"
+      className={`${
+        darkMode ? "bg-dark" : "bg-light"
+      } d-flex align-items-center justify-content-center`}
       style={{ minHeight: "100vh" }}
     >
       <div className="w-100" style={{ maxWidth: "400px" }}>
--- src/components/authentication/Login.js
+++ src/components/authentication/Login.js
@@ -3,6 +3,7 @@ import { Form, Button, Card, Alert } from "react-bootstrap";
 import { useAuth } from "../../context/AuthContext";
 import { Link, useHistory } from "react-router-dom";
 import CenteredContainer from "./CenteredContainer";
+import "../drive/style.css";
 
 export default function Login() {
   const emailRef = useRef();
@@ -11,6 +12,7 @@ export default function Login() {
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
   const history = useHistory();
+  const darkMode = JSON.parse(localStorage.getItem("darkMode"));
 
   async function handleSubmit(e) {
     e.preventDefault();
@@ -27,34 +29,38 @@ export default function Login() {
   }
 
   return (
-    <CenteredContainer>
-      <Card>
-        <Card.Body>
-          <h2 className="text-center mb-4">Sign In</h2>
-          {error && <Alert variant="danger">{error}</Alert>}
-          <Form onSubmit={handleSubmit}>
-            <Form.Group id="email">
-              <Form.Label>Email</Form.Label>
-              <Form.Control type="email" ref={emailRef} required />
-            </Form.Group>
-            <Form.Group id="password">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" ref={passwordRef} required />
-            </Form.Group>
+    <div className={darkMode ? "bg-dark" : "bg-light"}>
+      <CenteredContainer>
+        <Card className={darkMode ? "bg-dark text-white" : "bg-light"}>
+          <Card.Body>
+            <h2 className="text-center mb-4">Sign In</h2>
+            {error && <Alert variant="danger">{error}</Alert>}
+            <Form onSubmit={handleSubmit}>
+              <Form.Group id="email">
+                <Form.Label>Email</Form.Label>
+                <Form.Control type="email" ref={emailRef} required />
+              </Form.Group>
+              <Form.Group id="password">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" ref={passwordRef} required />
+              </Form.Group>
 
-            <Button disabled={loading} className="w-100 mt-3" type="submit">
-              Sign In
-            </Button>
-          </Form>
+              <Button disabled={loading} className="w-100 mt-3" type="submit">
+                Sign In
+              </Button>
+            </Form>
 
-          <div className="w-100 text-center mt-3">
-            <Link to="/forgot-password">Forgot Password?</Link>
-          </div>
-        </Card.Body>
-      </Card>
-      <div className="w-100 text-center m-2">
-        Need an account? <Link to="/signup">Sign Up</Link>
-      </div>
-    </CenteredContainer>
+            <div className="w-100 text-center mt-3">
+              <Link to="/forgot-password">Forgot Password?</Link>
+            </div>
+          </Card.Body>
+        </Card>
+        <div
+          className={`${darkMode ? "text-white" : null} w-100 text-center m-2`}
+        >
+          Need an account? <Link to="/signup">Sign Up</Link>
+        </div>
+      </CenteredContainer>
+    </div>
   );
 }
