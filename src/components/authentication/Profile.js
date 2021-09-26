586888d2f862173c8e537808e7775a861a6f7c4e
Harsh-0986 <harsh.r.shah2102@gmail.com>
2021-09-26 15:32:30 +0530

    Added dark mode to update profile screen

--- src/components/authentication/Profile.js
+++ src/components/authentication/Profile.js
@@ -4,11 +4,13 @@ import { Card, Button, Alert } from "react-bootstrap";
 import { useAuth } from "../../context/AuthContext";
 import { Link, useHistory } from "react-router-dom";
 import CenteredContainer from "./CenteredContainer";
+import "../drive/style.css";
 
 export default function Profile() {
   const [error, setError] = useState("");
   const { currentUser, logout } = useAuth();
   const history = useHistory();
+  const darkMode = JSON.parse(localStorage.getItem("darkMode"));
 
   async function handleLogout() {
     setError("");
@@ -22,24 +24,26 @@ export default function Profile() {
   }
 
   return (
-    <CenteredContainer>
-      <Card>
-        <Card.Body>
-          <h2 className="text-center mb-4">Profile</h2>
-          {error && <Alert variant="danger">{error}</Alert>}
-          <strong>Email: </strong>
-          {currentUser.email}
-          <Link to="/update-profile" className="btn btn-primary w-100 mt-3">
-            Update Profile
-          </Link>
-        </Card.Body>
-      </Card>
+    <div className={darkMode ? "bg-dark" : "bg-light"}>
+      <CenteredContainer>
+        <Card className={darkMode ? "bg-dark text-white" : "bg-light"}>
+          <Card.Body>
+            <h2 className="text-center mb-4">Profile</h2>
+            {error && <Alert variant="danger">{error}</Alert>}
+            <strong>Email: </strong>
+            {currentUser.email}
+            <Link to="/update-profile" className="btn btn-primary w-100 mt-3">
+              Update Profile
+            </Link>
+          </Card.Body>
+        </Card>
 
-      <div className="w-100 text-center m-2">
-        <Button variant="danger" onClick={handleLogout}>
-          Logout
-        </Button>
-      </div>
-    </CenteredContainer>
+        <div className="w-100 text-center m-2">
+          <Button variant="danger" onClick={handleLogout}>
+            Logout
+          </Button>
+        </div>
+      </CenteredContainer>
+    </div>
   );
 }
